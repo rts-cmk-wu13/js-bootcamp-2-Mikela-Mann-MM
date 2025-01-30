e2ee9a7f4e7f2428a7a6000ca2453ffdb181f818
mikelamann1@gmail.com <mikelamann1@gmail.com>
2025-01-30 11:10:35 +0100

    gennemgang af niveau1 -1 tilføjet

--- Opgaver/Niveau1/01/script.js
+++ Opgaver/Niveau1/01/script.js
@@ -1,4 +1,4 @@
-let colorButton = document.getElementById("my-button");
+let colorButton = document.getElementById("my-button"); 
 
 // De to farver, der skal skiftes mellem
 let colors = ["#eee", "#7f7"];
@@ -20,3 +20,42 @@ Index til at vælge farve: currentColorIndex bruges til at holde styr på, hvilk
 Event listener: Når knappen trykkes, bliver colorButton.style.backgroundColor sat til den nuværende farve (den farve, der svarer til currentColorIndex).
 Opdatering af farve: Efter at have ændret farven, bliver currentColorIndex opdateret til den næste værdi, og ved at bruge % colors.length (modulus) sikrer jeg, at det kun skifter mellem de to farver (0 og 1). Det betyder, at når currentColorIndex når 2, vil det blive nulstillet til 0, og processen begynder forfra.
 På denne måde kan man skifte mellem de to farver uden at bruge en if-sætning.*/
+
+
+
+// const button = document.querySelector("#my-button")
+
+// let isSelected = false;
+
+// function toggleButton (selected) {
+//     if (selected) {
+//         button.classList.add("click")
+//     } else {
+//         button.classList.remove("click")
+//     }
+// }
+
+// button.addEventListener("click", function(){
+
+// isSelected = ! isSelected
+
+// toggleButton(isSelected)
+
+//   if ( isSelected == true ) {
+//       isSelected = false
+//    } else {
+//       isSelected = true
+// }
+
+//   console.log(isSelected);
+    
+// })
+
+
+// button.addEventListener("click", function() {
+//   button.classList.toggle("click")
+// })
+
+// genvej til at udkommentere
+// shift - option - a
+// shift - command - 7 
